79ed3685d65becab0c57835c8bfb2d11675dbc05
Mateus Hernandes <mateushernandes939@gmail.com>
2023-05-02 17:54:40 -0300

    Update pixel.js

--- src/components/pixel.js
+++ src/components/pixel.js
@@ -1,9 +1,8 @@
 import React from 'react'
 
 export default () => 
- <React.Fragment>
-    <script>
-!function(f,b,e,v,n,t,s)
+<React.Fragment>
+    <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -12,9 +11,9 @@ t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '745939000345503');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=745939000345503&ev=PageView&noscript=1"
-/></noscript>
+fbq('track', 'PageView');` }}
+    />
+    <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=745939000345503&ev=PageView&noscript=1"
+    /></noscript>
   </React.Fragment>
